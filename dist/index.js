bae6a143aee1f396abd129179ebc26055fcb35be
Socheat Sok <socheatsok78@gmail.com>
2024-06-03 10:54:01 +0700

    Build action dist

--- dist/index.js
+++ dist/index.js
@@ -41992,7 +41992,7 @@ async function main() {
         __message += `, Dart SDK version: (v${release.version})`
       }
   
-      core.info(__message)
+      core.info(`${__message} the version satisfies the constraints in the pubspec.yaml file`)
       __message = "" // Reset the message
     }
   })
